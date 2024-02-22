904a724f05ff122c25e8c5cb78b423082cadfc9f
Jonas <jonas@Jonass-MBP-2.home>
2024-02-22 13:16:35 +0100

    small improvements on plotS3 method

--- R/plot.ClusterVAR.R
+++ R/plot.ClusterVAR.R
@@ -20,6 +20,7 @@ plot.ClusterVAR <- function(object,
 
   if(show == "Best-per-number-of-clusters") {
 
+    N_L <- nrow(out_table) # number of models
     K <- nrow(out_table)
     yrange <- range(c(out_table$ICL, out_table$BIC))
 
@@ -34,13 +35,15 @@ plot.ClusterVAR <- function(object,
     title("Models with Best Lag for each Cluster", font.main=1)
 
     # Plotting Data
+    points(1:N_L, out_table$ICL, col=cols[1], pch=19)
+    points(1:N_L, out_table$BIC, col=cols[2], lty=2, pch=17)
     lines(1:K, out_table$ICL, col=cols[1])
-    lines(1:K, out_table$BIC, col=cols[1], lty=2)
+    lines(1:K, out_table$BIC, col=cols[2], lty=2)
 
     # Legend
     legend("topright", legend=c("ICL", "BIC"),
            lty=1:2, col=cols[1:2], text.col=cols[1:2],
-           bty="n", cex=1.2)
+           bty="n", cex=1.2, pch=c(19, 17))
 
   } # end if
 
@@ -51,11 +54,9 @@ plot.ClusterVAR <- function(object,
 
     N_L <- nrow(out_table) # number of models
 
-    # TODO: Order table by complexity
+    # Cut x-axis labels out of rownames of summary table
     labels <- substr(rownames(out_table), 6, nchar(rownames(out_table)))
     yrange <- range(c(out_table$HQ, out_table$SC))
-
-    # Get labels ..
 
     # Plotting canvas
     par(mar=c(4.4, 5.5, 2, 1.2))
@@ -69,13 +70,15 @@ plot.ClusterVAR <- function(object,
     title(paste0("Lags Combinations for ", Number_of_Clusters, " Clusters"), font.main=1)
 
     # Data
+    points(1:N_L, out_table$HQ, col=cols[1], pch=19)
+    points(1:N_L, out_table$SC, col=cols[2], lty=2, pch=17)
     lines(1:N_L, out_table$HQ, col=cols[1])
     lines(1:N_L, out_table$SC, col=cols[2], lty=2)
 
     # Legend
     legend("topleft", legend=c("HQ", "SC"),
            lty=1:2, col=cols[1:2], text.col=cols[1:2],
-           bty="n", cex=1.2)
+           bty="n", cex=1.2, pch=c(19, 17))
 
 
 
