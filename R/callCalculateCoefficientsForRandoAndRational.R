56e9ede26666bd386185eb82161da4821ce11db3
AnieBee <anja-franziska-ernst@gmx.de>
2024-03-15 09:44:26 +0100

    replace with a simple parallelised function in calculateB for troubleshooting

--- R/callCalculateCoefficientsForRandoAndRational.R
+++ R/callCalculateCoefficientsForRandoAndRational.R
@@ -1,18 +1,18 @@
 callCalculateCoefficientsForRandoAndRational <- function(Covariates, K, N, nDepVar, qqq, HighestLag, LowestLag,
                                                          PersEnd, PersStart, Y, X, NewPredictableObs, LaggedPredictObs)
 {
-    
+
     CoeffsForRandoAndRationalList = vector(mode = "list", HighestLag) # list elements of 1 to LowestLag are empty
     for(lagCounter in LowestLag:HighestLag)
     {
-        CoeffsForRandoAndRationalList[[lagCounter]] = 
-            calculateCoefficientsForRandoAndRational(Covariates = Covariates, K = K, N = N,
-                                                     nDepVar = nDepVar, qqq = qqq, Lag = lagCounter, 
+        CoeffsForRandoAndRationalList[[lagCounter]] =
+          calculateCoefficientsForRandoAndRational(Covariates = Covariates, K = K, N = N,
+                                                     nDepVar = nDepVar, qqq = qqq, Lag = lagCounter,
                                                      PersEnd = PersEnd, PersStart = PersStart, Y = Y,
                                                      X = X, NewPredictableObs = NewPredictableObs[[lagCounter]],
                                                      LaggedPredictObs = LaggedPredictObs[[lagCounter]])
     }
-    
+
     invisible(CoeffsForRandoAndRationalList)
-    
+
 }
