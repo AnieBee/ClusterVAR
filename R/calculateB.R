ea053cda8ab55b0b9bd3524c37c8b0cb1a321066
AnieBee <anja-franziska-ernst@gmx.de>
2024-03-21 14:33:27 +0100

    update LCVAR

--- R/calculateB.R
+++ R/calculateB.R
@@ -1,16 +1,8 @@
-# Somtimes but not always faster than non-parallelized B
-# Not sure if the list that is returned is always in the same order in terms of 1:K, B[ , , j] has to return the B for the correct cluster
 calculateB <- function(Covariates, K, nDepVar, A, Sigma, N, NewPredictableObs, X, Y, Lags, FZY, qqq, B)
 {
     #if(Covariates == "equal-within-clusters"){# B(0): equal-within-clusters
-        cluster <- 6
-        cl <- makeCluster(cluster, outfile="")
-        registerDoParallel(cl)
-
-        B_List <- foreach(j = 1:K,
-                .packages = c("MASS")) %dopar%
+        for(j in 1:K)
         {
-
             Bnum = 0
             Bdenom = 0
             Delta = cbind(diag(nDepVar), (-1) * A[ , 1:(nDepVar * Lags[j]), j])
@@ -20,23 +12,22 @@ calculateB <- function(Covariates, K, nDepVar, A, Sigma, N, NewPredictableObs, X
                 Bn = 0
                 Bd = 0
                 for(trunner in c(NewPredictableObs[[ Lags[j] ]][[i]]) )
-                {
+                { 
                     XtildaKron <- kronecker(t(X[ , (trunner):(trunner - Lags[j]), drop = FALSE]),
                                             diag(nDepVar)) # drop = FALSE is needed here in case of q = 1
-                    Bd = Bd + ( t(XtildaKron) %*% NewCovaMat %*% XtildaKron )
-                    Bn = Bn + ( t(XtildaKron) %*% NewCovaMat %*%
-                                    as.vector(Y[ , (trunner):(trunner - Lags[j]), drop = FALSE]) )
+                    Bd = Bd + ( t(XtildaKron) %*% NewCovaMat %*% XtildaKron ) 
+                    Bn = Bn + ( t(XtildaKron) %*% NewCovaMat %*% 
+                                     as.vector(Y[ , (trunner):(trunner - Lags[j]), drop = FALSE]) )
                 }
                 Bnum = Bnum + (FZY[ i, j] * Bn)
                 Bdenom = Bdenom + (FZY[ i, j] * Bd)
             }
             BasVec = MASS::ginv(Bdenom) %*% Bnum
-            return(matrix(BasVec, nrow = nDepVar, ncol = qqq, byrow = FALSE))
+            B[, , j] = matrix(BasVec, nrow = nDepVar, ncol = qqq, byrow = FALSE)
         }
     #}
-    stopCluster(cl)
-    for(j in 1:K) B[, , j] = B_List[[j]]
-
+    
     invisible(B)
-
+    
 }
+
