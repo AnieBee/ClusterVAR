7a85165bb38eba2a0f61efed2a08be00c1f63fa4
AnieBee <anja-franziska-ernst@gmx.de>
2024-07-05 10:12:40 +0200

    update numberPredictableObservations to return number of predictable observations per person

--- R/numberPredictableObservations.R
+++ R/numberPredictableObservations.R
@@ -130,6 +130,12 @@ numberPredictableObservations <- function(Data,
 
     }
 
+    names(Tni_NPred) = apply(as.matrix(1:HighestLag), 1, function(x) paste(c(x, "Lag"), collapse = " ")) # check this works for lag combis
+    for (lagRunner in LowestLag:HighestLag)
+    {
+      names(Tni_NPred[[lagRunner]]) = pers
+    }
+
     FunctionOutput = data.frame(matrix(NA, nrow = HighestLag, ncol = 1),
                                 row.names = apply(as.matrix(1:HighestLag), 1, function(x) paste(c(x, "Lag"), collapse = " ")))
     colnames(FunctionOutput) = c("Total predictable observations")
@@ -139,8 +145,10 @@ numberPredictableObservations <- function(Data,
         FunctionOutput[lagRunner, ] = sum(Tni_NPred[[lagRunner]])
     }
     FunctionOutput = FunctionOutput[LowestLag:HighestLag, , drop = FALSE]
-    #class(FunctionOutput) <- c("ClusterVARNumberPredictable", class(FunctionOutput))
 
-    return(FunctionOutput)
+    OutList = list(Tni_NPred, FunctionOutput)
+    names(OutList) = c("Predictable observations per subject", "Total predictable observations")
+
+    return(OutList)
 
 }
