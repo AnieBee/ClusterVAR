c091ececbc0869e99db4b86c9e5b6ce9fff830ea
AnieBee <anja-franziska-ernst@gmx.de>
2024-07-08 10:26:12 +0200

    fix checkPosteriorsNA to indicate when iterations have been reset

--- R/checkPosteriorsNA.R
+++ R/checkPosteriorsNA.R
@@ -1,6 +1,7 @@
 
 checkPosteriorsNA <- function(FZY, K)
 {
+    iterationReset = FALSE
     if(any(is.na(FZY)))
     { # FZY = ifelse(is.na(FZY), 1/K, FZY)
       # if posterior prob for any person is NA reset that posterior tau_{ij} to a "blind" prior: 1/k
@@ -8,10 +9,11 @@ checkPosteriorsNA <- function(FZY, K)
         #cat("\n Warning: Some posteriors are NA in EM-iteration", EMiteration, ", memberships reset \n")
         FZY[which(is.na(FZY))] = 1 / K # Set those p(Y|z_{ik}) that are NA for some person to 1/K
         FZY = t(scale(t(FZY), center = FALSE, scale = rowSums(FZY))) # Scale posteriors so they sum to 1 again
+        iterationReset = TRUE
     }
 
     stopifnot(sum(FZY) != 0) # sum(FZY should equal N)
 
-    invisible(list(FZY = FZY, iterationReset = any(is.na(FZY))))
+    invisible(list(FZY = FZY, iterationReset = iterationReset))
 
 }
--- R/checkSingularitySigma.R
+++ R/checkSingularitySigma.R
@@ -1,17 +1,17 @@
 checkSingularitySigma <- function(nDepVar, K, Sigma) {
     iterationReset <- FALSE
-    
+
     # Check for singularity using a vectorized approach
     singular_components <- sapply(1:K, function(j) det(Sigma[, , j]) < 1.0e-200)
-    
+
     # Identify the singular components
     singular_indices <- which(singular_components)
-    
+
     # Reset the diagonals for singular components
-    Sigma[, , singular_indices] <- Sigma[, , singular_indices] + 0.01
-    
+    if(any(singular_components)) Sigma[, , singular_indices] <- Sigma[, , singular_indices] + 0.01
+
     # Update iterationReset flag
     iterationReset <- any(singular_components)
-    
+
     invisible(list(Sigma = Sigma, iterationReset = iterationReset))
 }
